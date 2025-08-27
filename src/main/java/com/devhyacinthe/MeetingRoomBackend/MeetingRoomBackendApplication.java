960c21fc1f46daf616ddfc38ddef08c97a93965a
devhyacinthe <yacinthedalmeida2002@gmail.com>
2025-08-27 14:31:43 +0000

    Init commit

---
+++ src/main/java/com/devhyacinthe/MeetingRoomBackend/MeetingRoomBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devhyacinthe.MeetingRoomBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRoomBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingRoomBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devhyacinthe/MeetingRoomBackend/MeetingRoomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhyacinthe.MeetingRoomBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingRoomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
